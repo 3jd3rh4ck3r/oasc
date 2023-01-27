a65282cb2effb6967e144410ea7dcb736e1af32b
z0nd3rl1ng <z0nd3rl1ng@localhost.localdomain>
2023-01-27 05:25:54 +0100

    fresh template

--- oasc.py
+++ oasc.py
@@ -1,9 +1,5 @@
 """-------------------------------------------------------------------
-<<<<<<< HEAD
 # OASC- OpenAI Security Console
-=======
-# OAC- OpenAI Console
->>>>>>> refs/remotes/origin/main
 -------------------------------------------------------------------"""
 __author__ = "z0nd3rl1ng" + "0xAsFi"
 __version__ = "0.0.1"
@@ -27,11 +23,6 @@ ENGINE = "text-davinci-002"
 TEMPERATURE = 0
 MAX_TOKENS = 2048
 """----------------------------------------------------------------"""
-<<<<<<< HEAD
-=======
-
-
->>>>>>> refs/remotes/origin/main
 # FUNCTION TO SET OPEN AI API KEY AS ENVIRONMENT VARIABLE
 def setEnvKey():
     token = input("[OpenAI API Key]╼> ")
@@ -47,15 +38,9 @@ def setFinetuning():
     MAX_TOKENS = int(input("[Set Max Tokens]╼> ")) # SET MAX_TOKENS
 
 # FUNCTION FOR THE OPENAI QUERY PROMPT
-<<<<<<< HEAD
 def openaiSecurityConsole():
     while True:
         interact = input("[OASC]╼> ")
-=======
-def openaiConsole():
-    while True:
-        interact = input("[OAC]╼> ")
->>>>>>> refs/remotes/origin/main
         if interact == "exit":
             exit()
         elif interact == "fine-tuning":
@@ -65,15 +50,9 @@ def openaiConsole():
         elif interact == "help":
             print("\nCOMMANDS\tDESCRIPTION\n")
             print("banner\t\tprint banner")
-<<<<<<< HEAD
             print("set api-key\texport OpenAI API-Key to environment")
             print("fine-tuning\tconfiguration menu for fine-tuning queries")
             print("exit\t\tquit oasc\n")
-=======
-            print("set api-key\tsave your OpenAI API key as environment variable")
-            print("fine-tuning\tlaunch configuration menu for fine-tuning")
-            print("exit\t\tquit oac\n")
->>>>>>> refs/remotes/origin/main
         elif interact == "banner":
             banner()
         else:
@@ -95,21 +74,14 @@ def banner():
 	A = [[' ','┌','─','┐'],
 	     [' ','├','─','┤'],
 	     [' ','┴',' ','┴']]
-<<<<<<< HEAD
 	S = [[' ','┌','─','┐'],
 	     [' ','└','─','┐'],
 	     [' ','└','─','┘']]
-=======
->>>>>>> refs/remotes/origin/main
 	C = [[' ','┌','─','─'],
 	     [' ','│',' ',' '],
 	     [' ','└','─','─']]
 	
-<<<<<<< HEAD
 	banner = [O,A,S,C]
-=======
-	banner = [O,A,C]
->>>>>>> refs/remotes/origin/main
 	final = []
 	print('\r')
 	init_color = random.randint(10,40)
@@ -133,16 +105,8 @@ def banner():
 		if charset < 2: final.append('\n   ')
 
 	print(f"   {''.join(final)}")
-<<<<<<< HEAD
 	print(f'{padding}  by z0nd3rl1ng & \n\t 0xAsFi\n')
 
 if __name__ == "__main__":
     banner()
     openaiSecurityConsole()
-=======
-	print(f'{padding}by z0nd3rl1ng & \n       0xAsFi\n')
-
-if __name__ == "__main__":
-    banner()
-    openaiConsole()
->>>>>>> refs/remotes/origin/main
