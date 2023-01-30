c134e194e2858986e1bf56ad61be6c920f621ade
z0nd3rl1ng <z0nd3rl1ng@localhost.localdomain>
2023-01-30 04:49:20 +0100

    back to origin

--- oasc.py
+++ oasc.py
@@ -37,15 +37,6 @@ def setFinetuning():
     ENGINE = input("[Select Engine]╼> ") # SET ENGINE
     TEMPERATURE = float(input("[Set Temperature]╼> ")) # SET TEMPERATURE
     MAX_TOKENS = int(input("[Set Max Tokens]╼> ")) # SET MAX_TOKENS
-
-def content():
-    def analyzer():
-        print("it works!")
-    def creator():
-        print("it works!")
-            
-    analyzer()
-    creator()
   
 # FUNCTION TO LIST HELP MENU - COULD BE SWAGGED UP ;)
 def help():
@@ -64,8 +55,6 @@ def openaiSecurityConsole():
             exit()
         elif interact == "fine-tuning":
             setFinetuning()
-        elif interact == "content":
-            content()
         elif interact == "api-key":
             setEnvKey()
         elif interact == "help":
